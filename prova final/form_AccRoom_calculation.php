e4caf5764d121e6d84d8dc0a9f697959612db8d1
Antoni <inotna.21@gmail.com>
2020-12-10 20:58:32 +0100

    Exam December

---
+++ prova final/form_AccRoom_calculation.php
@@ -0,0 +1,47 @@
+<?php
+//calcular el preu del tipus d'habitació i retornar un valor numeric
+//si es single el preu es 30
+//si es double el preu es 50
+//en cas contrari el preu es 0
+//emprar la funcio strcmp vista als exercicis de classe
+function typePriceCalculation($type){
+if (!strcmp($type, 'single'))
+  return 30;
+elseif (!strcmp($type, 'double'))
+  return 50;
+return 0;
+}
+
+//calcular el preu segons el genere d'habitació i retornar un valor numeric
+//si es female o male el preu es 10
+//si es mixed el preu es 5
+//en cas contrari el preu es 0
+//emprar la funcio strcmp vista als exercicis de classe
+function genderPriceCalculation($gender){
+  if (!strcmp($gender, 'female')||!strcmp($gender, 'male'))
+    return 10;
+  elseif (!strcmp($gender, 'mixed'))
+    return 5;
+  return 0;
+}
+
+//calcular el preu segons les vistes d'habitació i retornar un valor numeric
+//si s'ha marcat amb vistes (yes) el preu es 15
+//en cas contrari el preu es 0
+//emprar la funcio strcmp vista als exercicis de classe
+function viewsPriceCalculation($views){
+if (!strcmp($views, 'yes'))
+  return 15;
+return 0;
+}
+
+//calcular el preu segons els serveis d'habitació seleccionats i retornar un valor numeric
+//per cada servei seleccionat el preu s'incrementa 20
+//example: si s'han seleccionat 3 serveis de restauració el preu es 20 multiplicat per 3
+//si no s'ha seleccionat cap el preu es 0
+//emprar una de les dues funcions vistes a classe per a saber quants elements hi ha a un array
+function foodServicesPriceCalculation($foodServices){
+    return (count($foodServices)*20);
+}
+
+?>
